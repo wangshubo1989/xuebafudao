8a8b9108d786991b13ff7194f299b5bf207452a9
hejiayi <janreyho@gmail.com>
2016-08-15 19:47:44 +0800

    logging.conf

--- example.py
+++ example.py
@@ -1,14 +1,18 @@
 import logging
+import logging.config  
 import os
 from eve.flaskapp import Eve
 from eve_tokenauth.eveapp import EveWithTokenAuth
 
-logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
+
+logging.config.fileConfig('logging.conf')  
+root_logger = logging.getLogger('root')  
+root_logger.debug('root logger...')  
+
 log = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     settings = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'settings.py')
-
     apiapp = Eve(settings=settings)
     evewta = EveWithTokenAuth(apiapp)
     apiapp.debug = True
