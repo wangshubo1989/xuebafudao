095b877ee5ff175a7aa58279ad3f28f2c8965af5
hejiayi <janreyho@gmail.com>
2016-08-15 21:12:06 +0800

    创建云信ID: createUserId. OK

--- example.py
+++ example.py
@@ -1,9 +1,16 @@
+#! /usr/bin/env python
+# coding=utf-8
 import logging
 import logging.config  
 import os
 from eve.flaskapp import Eve
 from eve_tokenauth.eveapp import EveWithTokenAuth
+import ServerAPI
+import neteaseIM
 
+srv = ServerAPI.ServerAPI(neteaseIM.AppKey, neteaseIM.AppSecret)
+token=srv.createUserId("57b177d89d7d9b5c12c30d4b")
+print token
 
 logging.config.fileConfig('logging.conf')  
 root_logger = logging.getLogger('root')  
