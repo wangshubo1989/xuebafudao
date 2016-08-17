6c017c261db24e27d8b2964242ed4d68ff292493
hejiayi <janreyho@gmail.com>
2016-08-17 20:31:06 +0800

    验证老系统用户成功：select uid from auth by token, select username from users by uid

--- eve_tokenauth/auth/token.py
+++ eve_tokenauth/auth/token.py
@@ -3,6 +3,7 @@ from eve.auth import TokenAuth
 import jwt
 from flask import current_app as app, request, abort
 from ServerAPI import neteaseIMsrv
+import MySQLdb
 
 class TokenAuthentication(TokenAuth):
 
@@ -23,8 +24,31 @@ class TokenAuthentication(TokenAuth):
 
         token = request.headers.get('Authorization')
 
+        username = getmysql_token(token)
+        if username:
+            return username
+
         return token and self.check_auth(token, allowed_roles, resource,
                                         method)
+
+def getmysql_token(token):
+    mysqldb = MySQLdb.connect("192.168.0.2","xueba","Xue-83177","xuebaedu")
+    cursor = mysqldb.cursor()
+
+    userid = 0
+    username = ""
+
+    sql = "SELECT uid FROM auth where token = " + " '" + token[6:] + "' "
+    cursor.execute(sql)
+    for row in cursor.fetchall():
+        userid = row[0]
+
+    sql = "SELECT username FROM users where uid = " + str(userid)
+    cursor.execute(sql)
+    for row in cursor.fetchall():
+        username = row[0]
+
+    return username
 
 
 def parse_token(req):
