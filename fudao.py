3744ec8de380696bd522dedc1cd145e5302f3518
hejiayi <janreyho@gmail.com>
2016-08-20 15:34:29 +0800

    add flastk-admin, test OK

--- fudao.py
+++ fudao.py
@@ -5,7 +5,23 @@ import logging.config
 import os
 from eve.flaskapp import Eve
 from tokenauth.eveapp import EveWithTokenAuth
+import flask_admin as admin
 
+# Create custom admin view
+class MyAdminView(admin.BaseView):
+    @admin.expose('/')
+    def index(self):
+        return self.render('myadmin.html')
+
+
+class AnotherAdminView(admin.BaseView):
+    @admin.expose('/')
+    def index(self):
+        return self.render('anotheradmin.html')
+
+    @admin.expose('/test/')
+    def test(self):
+        return self.render('test.html')
 
 logging.config.fileConfig('config/logging.conf')   
 logger = logging.getLogger('main')  
@@ -15,8 +31,23 @@ logger.info('main logger start')
 if __name__ == '__main__':
 
 
-    settings = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'settings.py')
-    apiapp = Eve(settings=settings)
-    evewta = EveWithTokenAuth(apiapp)
-    apiapp.debug = True
-    apiapp.run()
+	settings = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'settings.py')
+	apiapp = Eve(settings=settings,template_folder='/Users/hejiayi/Desktop/python/eve/fudao/templates')
+
+	# Flask views
+	@apiapp.route('/')
+	def index():
+	    return '<a href="/admin/">Click me to get to Admin!</a>'
+	@apiapp.route('web/')
+    def index():
+        return self.render('myadmin.html')
+
+	# Create admin interface
+	admin = admin.Admin(name="Example: Simple Views", template_mode='bootstrap3')
+	admin.add_view(MyAdminView(name="view1", category='Test'))
+	admin.add_view(AnotherAdminView(name="view2", category='Test'))
+	admin.init_app(apiapp)
+
+	evewta = EveWithTokenAuth(apiapp)
+	apiapp.debug = True
+	apiapp.run()
