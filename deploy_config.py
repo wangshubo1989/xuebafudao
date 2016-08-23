a351a1bfe9a67bb75082ee55fd9927a023178274
hejiayi <janreyho@gmail.com>
2016-08-23 16:25:05 +0800

    gunicorn

---
+++ deploy_config.py
@@ -0,0 +1,46 @@
+# import os
+# import sys
+# bind='127.0.0.1:8001' 
+# workers=4 
+# backlog=2048
+# debug=True
+# proc_name='gunicorn.pid'
+# pidfile='/var/log/gunicorn/gunicorn.pid'
+# accesslog='/var/log/gunicorn/gunicorn.log'
+# loglevel='debug'
+
+# coding=utf-8
+import sys
+import os
+import multiprocessing
+
+disable_existing_loggers = False
+path_of_current_file = os.path.abspath(__file__)
+path_of_current_dir = os.path.split(path_of_current_file)[0]
+
+_file_name = os.path.basename(__file__)
+
+sys.path.insert(0, path_of_current_dir)
+
+
+worker_class = 'sync'
+workers = multiprocessing.cpu_count()
+
+chdir = path_of_current_dir
+
+worker_connections = 1000
+timeout = 30
+max_requests = 2000
+graceful_timeout = 30
+
+loglevel = 'info'
+
+reload = True
+debug = False
+
+
+
+bind = "%s:%s" % ("0.0.0.0", 8002)
+pidfile = '%s/run/%s.pid' % (path_of_current_dir, _file_name)
+errorlog = '%s/logs/%s_error.log' % (path_of_current_dir, _file_name)
+accesslog = '%s/logs/%s_access.log' % (path_of_current_dir, _file_name)
--- fudao.py
+++ fudao.py
@@ -30,50 +30,43 @@ logging.config.fileConfig('config/logging.conf')
 logger = logging.getLogger('main')  
 logger.info('main logger start')  
 
+SETTINGS_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'settings.py')
+apiapp = Eve(settings=SETTINGS_PATH, template_folder='/Users/hejiayi/Desktop/python/eve/fudao/templates')
 
+# Flask views
+@apiapp.route('/')
+def index():
+    return '<a href="/admin/">Click me to get to Admin!</a>'
+
+@apiapp.route('/web')
+def webindex():
+    return render_template('/web/mycourse.html')
+
+admin = admin.Admin(name="Example: Simple Views", template_mode='bootstrap3')
+admin.add_view(MyAdminView(name="view1", category='Test'))
+admin.add_view(AnotherAdminView(name="view2", category='Test'))
+admin.init_app(apiapp)
+
+# /api-docs
+apiapp.register_blueprint(swagger)
+apiapp.config['SWAGGER_INFO'] = {
+    'title': '学吧辅导 API',
+    'version': '0.1',
+    'description': '一对一辅导，提供实时语音，和教学白板。',
+    'termsOfService': 'my terms of service',
+    'contact': {
+        'name': 'hejiayi',
+        'url': 'http://192.168.0.2:5000'
+    },
+
+}
+apiapp.config['SWAGGER_HOST'] = '192.168.0.2:5000'
+
+evewta = EveWithTokenAuth(apiapp)
+apiapp.debug = True
+
+# werkzeug_logger = logging.getLogger('werkzeug')
+# werkzeug_logger.setLevel(DEBUG)
 if __name__ == '__main__':
-
-
-	settings = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'settings.py')
-	apiapp = Eve(settings=settings,template_folder='/Users/hejiayi/Desktop/python/eve/fudao/templates')
-
-	# Flask views
-	@apiapp.route('/')
-	def index():
-	    return '<a href="/admin/">Click me to get to Admin!</a>'
-
-	@apiapp.route('/web')
-	def webindex():
-	    return render_template('web/mycourse.html')
-
-	# Create admin interface
-	admin = admin.Admin(name="Example: Simple Views", template_mode='bootstrap3')
-	admin.add_view(MyAdminView(name="view1", category='Test'))
-	admin.add_view(AnotherAdminView(name="view2", category='Test'))
-	admin.init_app(apiapp)
-
-	# add eve-swagger
-	# /api-docs
-	# curl -k -H "Authorization: Bearer tnIpW1XWyrrUQSlIUZbG2O2TNjp3W7" https://localhost:5000/api-docs
-	apiapp.register_blueprint(swagger)
-	# required. See http://swagger.io/specification/#infoObject for details.
-	apiapp.config['SWAGGER_INFO'] = {
-	    'title': '学吧辅导 API',
-	    'version': '0.1',
-	    'description': '一对一辅导，提供实时语音，和教学白板。',
-	    'termsOfService': 'my terms of service',
-	    'contact': {
-	        'name': 'hejiayi',
-	        'url': 'http://192.168.0.2:5000'
-	    },
-	    # 'license': {
-	    #     'name': 'BSD',
-	    #     'url': 'https://github.com/nicolaiarocci/eve-swagger/blob/master/LICENSE',
-	    # }
-	}
-	# optional. Will use flask.request.host if missing.
-	apiapp.config['SWAGGER_HOST'] = '192.168.0.2:5000'
-
-	evewta = EveWithTokenAuth(apiapp)
-	apiapp.debug = True
+	# apiapp.debug = True
 	apiapp.run()
