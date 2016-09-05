6de87427e275ca08ffc4be6c7a44cc80f315067f
hejiayi <janreyho@gmail.com>
2016-09-05 21:42:34 +0800

    /var/log/fudao/main.log

--- deploy_config.py
+++ deploy_config.py
@@ -18,18 +18,18 @@ import os
 import multiprocessing
 
 disable_existing_loggers = False
-path_of_current_file = os.path.abspath(__file__)
-path_of_current_dir = os.path.split(path_of_current_file)[0]
+# path_of_current_file = os.path.abspath(__file__)
+# path_of_current_dir = os.path.split(path_of_current_file)[0]
 
 _file_name = os.path.basename(__file__)
 
-sys.path.insert(0, path_of_current_dir)
+# sys.path.insert(0, path_of_current_dir)
 
 
 worker_class = 'sync'
 workers = multiprocessing.cpu_count()
 
-chdir = path_of_current_dir
+# chdir = path_of_current_dir
 
 worker_connections = 1000
 timeout = 30
@@ -44,6 +44,7 @@ debug = False
 
 
 bind = "%s:%s" % ("0.0.0.0", 5000)
-pidfile = '%s/run/%s.pid' % (path_of_current_dir, _file_name)
-errorlog = '%s/logs/%s_error.log' % (path_of_current_dir, _file_name)
-accesslog = '%s/logs/%s_access.log' % (path_of_current_dir, _file_name)
+pidfile = '/var/run/fudao.pid'
+# pidfile = '%s/run/%s.pid' % (path_of_current_dir, _file_name)
+# errorlog = '%s/logs/%s_error.log' % (path_of_current_dir, _file_name)
+# accesslog = '%s/logs/%s_access.log' % (path_of_current_dir, _file_name)
